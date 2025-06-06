7d9227afda0b7041af509ba17f5c4efcf7de7120
alex kamau <alex.kamau1@student.moringaschool.com>
2025-06-06 14:02:31 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,22 @@
+const myFunction = function() {
+  return 'Function expression!';
+};
+
+const add = (a, b) => a + b;
+
+const square = x => x * x;
+
+const divide = function() {
+  return 2000 / 100;
+};
+
+const logAndAdd = (a, b) => {
+  console.log(a);
+  console.log(b);
+  return a + b;
+};
+
+const nums = [1, 2, 3];
+const doubled = nums.map(num => num * 2);
+
+
